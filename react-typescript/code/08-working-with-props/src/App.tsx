451c244d6cd105c6a557a19c95ede3687cc2ad92
Stanislav Chvila <schvila@gmail.com>
2021-08-04 18:12:10 +0200

    minor changes sync

--- react-typescript/code/08-working-with-props/src/App.tsx
+++ react-typescript/code/08-working-with-props/src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Todos from './components/Todos';
 
 function App() {
--- react-typescript/code/08-working-with-props/src/components/Todos.tsx
+++ react-typescript/code/08-working-with-props/src/components/Todos.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 const Todos: React.FC<{ items: string[] }> = (props) => {
   return (
